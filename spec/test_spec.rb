c5c690fb1956c90808ca3ebb2473041ee0823b92
Thomas Hawley <thawley22@turing.edu>
2023-08-31 12:23:19 -0600

    fix: Add factory bot configuration to rails helper

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 RSpec.describe 'Test' do
   describe 'test' do
     it 'testing factories', :vcr do
-      book = FactoryBot.create_list(:book, 5)
-      library = FactoryBot.create(:library)
+      book = create_list(:book, 5)
+      library = create(:library)
       # Write some expects
     end
 
     it 'a test for the test, testing factories', :vcr do
-      libraries = FactoryBot.create_list(:library, 5)
-      books = FactoryBot.create_list(:book, 10)
+      libraries = create_list(:library, 5)
+      books = create_list(:book, 10)
 
       library_books = libraries.each do |library|
         books.sample(rand(1..5)).each do |book|
-          FactoryBot.create(:library_book, library: library, book: book)
+          create(:library_book, library: library, book: book)
         end
       end
       # Write some expects
