8ccf5a54381f2b45ea7f74060b75483573992a2a
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-09-02 15:26:21 -0600

    test: Write model tests for library.

---
+++ spec/models/library_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Library, type: :model do
+  describe 'relatioships' do
+    it { should have_many(:library_books) }
+    it { should have_many(:books).through(:library_books) }
+  end
+end
