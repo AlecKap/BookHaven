224ba8fb63907635a09f069e4cd7e25524d452c0
Thomas Hawley <thawley22@turing.edu>
2023-09-13 21:08:12 -0600

    test: Add test for library show page

--- spec/features/libraries/show_spec.rb
+++ spec/features/libraries/show_spec.rb
@@ -9,23 +9,59 @@ RSpec.describe 'Library Show Page' do
       end
     end
 
-      it 'I see the BookHaven name and it is a link to the root_path' do
-        expect(page).to have_link("BookHaven")
-        
-        click_link("BookHaven")
-        
-        expect(current_path).to eq(root_path)
-      end
+    it 'I see the BookHaven name and it is a link to the root_path' do
+      expect(page).to have_link("BookHaven")
+      
+      click_link("BookHaven")
+      
+      expect(current_path).to eq(root_path)
+    end
 
-      it 'I see the name and address of the library' do
-        expect(page).to have_content(@library.name)
-        expect(page).to have_content(@library.full_address)
-      end
+    it 'I see the name and address of the library' do
+      expect(page).to have_content(@library.name)
+      expect(page).to have_content(@library.full_address)
+    end
 
-      it 'I see a button to Add a book to this library' do
-        expect(page).to have_button("Add a Book")
+    it 'I see a button Return to Libraries that takes me to the library index page' do
+      expect(page).to have_button("Return to Libraries")
+      
+      click_button("Return to Libraries")
 
-        
+      expect(current_path).to eq(libraries_path)
+    end
+
+    it 'I see a button to Add a book to this library' do
+      expect(page).to have_button("Add a Book")
+    end
+
+    it 'When I click the Add a Book button a form appears to add a books ISBN' do
+      click_button("Add a Book")
+      
+      expect(page).to have_field(:isbn)
+      expect(page).to have_button("Add Book")
+      expect(page).to have_button("Cancel")
+      expect(page).to_not have_button("Add a Book")
+
+      click_button("Cancel")
+
+      expect(page).to_not have_field(:isbn)
+      expect(page).to_not have_button("Add Book")
+      expect(page).to have_button("Add a Book")
+    end
+
+    it 'I see a list of all the books images at this library and each image is a link' do
+      @library.books.each do |book|
+        within("#book_#{book.id}") do
+          expect(page).to have_css("img[src*='#{book.book_img}']")
+          expect(page).to have_link('', href: library_book_path(@library, book))
+        end
       end
+    end
+
+    it 'When I click on a book image, I am taken to that books show page' do
+      click_link('', href: library_book_path(@library, @library.books.first))
+
+      expect(current_path).to eq(library_book_path(@library, @library.books.first))
+    end
   end
 end
